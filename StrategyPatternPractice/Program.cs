7e1f95a4cdf11aaa7fd2dec0cb2aa351156cd197
gary.lai99 <gary.lai99@TI03NB133>
2019-04-14 15:43:20 +0800

    first commit

--- StrategyPatternPractice/Program.cs
+++ StrategyPatternPractice/Program.cs
@@ -1,21 +1,106 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StrategyPatternPractice
+namespace StrategyPattern
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var caculator = new Caculator();
+            caculator.SetStrategy(Caculator.Dotype.Multyply);
+            var multyplyResult = caculator.Execute(3, 4);
+            caculator.Reset();
+            caculator.SetStrategy(Caculator.Dotype.Add);
+            var addResult = caculator.Execute(4, 5);
+            Console.WriteLine($"Multyply:{multyplyResult}, Add:{addResult}");
+        }
+    }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+    //1.先定義一個介面，用這個介面定義一系列演算法
+    public interface IStrategy
+    {
+        int Caculate(int a, int b);
+    }
+
+    //2.將加減乘除等一系列的算法實現出來
+    public class Add : IStrategy
+    {
+        public int Caculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Minus : IStrategy
+    {
+        public int Caculate(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    public class Multyply : IStrategy
+    {
+        public int Caculate(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Divide : IStrategy
+    {
+        public int Caculate(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    //3.用一個類裝這個算法，並且用簡單工廠模式封裝一下
+    public class Caculator
+    {
+        private int now;
+        private IStrategy strategy;
+
+        //策略模式
+        public int Execute(int a, int b)
+        {
+            return strategy.Caculate(a, b);
+        }
+
+        public void Reset()
+        {
+            now = 0;
+        }
+
+        //簡單工廠模式
+        public void SetStrategy(Dotype dotype)
+        {
+            switch (dotype)
+            {
+                case Dotype.Add:
+                    strategy = new Add();
+                    break;
+
+                case Dotype.Minus:
+                    strategy = new Minus();
+                    break;
+
+                case Dotype.Multyply:
+                    strategy = new Multyply();
+                    break;
+
+                case Dotype.Divide:
+                    strategy = new Divide();
+                    break;
+            }
+        }
+
+        public enum Dotype
+        {
+            Add,
+            Minus,
+            Multyply,
+            Divide
         }
     }
 }
